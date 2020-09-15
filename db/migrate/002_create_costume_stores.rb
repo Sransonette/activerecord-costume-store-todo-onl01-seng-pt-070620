01fd14bd74f80abb5e667ad498f76cebb480b87b
Sean Ransonette <sransonette@gmail.com>
2020-09-15 01:24:00 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,7 +10,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
-      t.datetime :closeing_time 
+      t.datetime :closing_time 
     end
   end
   
